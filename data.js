9f0a9e0a7c3ce6ea5d9cf271e6a9778762475379
Enriquezwere <enriquez.marieleanne@gmail.com>
2023-07-06 18:16:27 -0800

    main-entrance

---
+++ data.js
@@ -0,0 +1,465 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-entrance",
+      "name": "Main Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3046454387405326,
+          "pitch": -0.07926994851467839,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.502221181240909,
+          "pitch": -0.18839193948785393,
+          "title": "CSTC Main Entrance",
+          "text": "CSTC main entrance is the primary point of entry for students, staff, and visitors to access the school premises.<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9774300203582467,
+          "pitch": 0.20082665876477535,
+          "rotation": 0,
+          "target": "0-main-entrance"
+        },
+        {
+          "yaw": -1.5515061961870416,
+          "pitch": 0.149873493301385,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.4467638635195437,
+          "pitch": 0.030606423716477593,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1541236963677708,
+          "pitch": -0.049484776466723446,
+          "title": "CSTC Clinic",
+          "text": "CSTC clinic is a designated area within the school where students can receive medical care, first aid, and health-related support."
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0574118113781488,
+          "pitch": 0.2245547896950164,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6021798329036585,
+          "pitch": 0.056084992307763315,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.07939162183682669,
+          "pitch": -0.03531600085653608,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0084778028533226,
+          "pitch": 0.1367277041863062,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.008974138009588017,
+          "pitch": -0.010931863166897315,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.48878930349691885,
+          "pitch": 0.010957745876222447,
+          "rotation": 0,
+          "target": "6-cashier-and-registrar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1184297400052436,
+          "pitch": 0.05860638650691641,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.2555224458653136,
+          "pitch": 0.08960085768203996,
+          "rotation": 0,
+          "target": "6-cashier-and-registrar"
+        },
+        {
+          "yaw": 0.04451997782711459,
+          "pitch": 0.09302492687671915,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cashier-and-registrar",
+      "name": "Cashier and Registrar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7060371387969866,
+          "pitch": 0.18721547819158246,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.7060371387969866,
+          "pitch": 0.18721547819158246,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.7060371387969866,
+          "pitch": 0.18721547819158246,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.7758598652120376,
+          "pitch": 0.1404536004062038,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.5819699399100706,
+          "pitch": 0.12092714124935533,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.600411446411691,
+          "pitch": -0.09292503871257729,
+          "title": "CSTC Cashier and Registrar",
+          "text": "CSTC cashier in a private school handles financial transactions and manages the school's finances, while the registrar is responsible for managing student records and overseeing the enrollment process.<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1155271175877814,
+          "pitch": 0.0583222032376387,
+          "rotation": 0,
+          "target": "6-cashier-and-registrar"
+        },
+        {
+          "yaw": -1.0524891487599035,
+          "pitch": 0.07906934449939484,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.775140099519529,
+          "pitch": 0.04567440433707759,
+          "title": "CSTC Canteen Entrance",
+          "text": "Canteen is a designated area within a school or institution where food and beverages are sold to students and staff.<br>"
+        },
+        {
+          "yaw": 2.6916901126208383,
+          "pitch": -0.01115675345488043,
+          "title": "CSTC Cashier and Registrar",
+          "text": "CSTC cashier in a private school handles financial transactions and manages the school's finances, while the registrar is responsible for managing student records and overseeing the enrollment process."
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.215113228619648,
+          "pitch": 0.22210769071818248,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.959639122075492,
+          "pitch": 0.23993767831328583,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CSTC Virtual Campus Tour Entrance",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
